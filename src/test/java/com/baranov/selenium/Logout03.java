b5f3c380af4dc2e271ea478f3c8ffaf01d5a7ddd
baranov.r.p@gmail.com <baranov.r.p@gmail.com@36abeab5-8eda-1f70-f883-9d943209e12b>
2013-05-01 15:42:44 +0000

    tests for 3 lesson, using XPath

---
+++ src/test/java/com/baranov/selenium/Logout03.java
@@ -0,0 +1,46 @@
+package com.baranov.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Logout03 extends com.baranov.selenium.pages.TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testLogout03() throws Exception {    driver.get(baseUrl + "/php4dvd/");
+    driver.findElement(By.xpath("//div[@id='wrapper']//div/nav//li/a[contains(@href,'logout')]")).click();
+    assertTrue(closeAlertAndGetItsText().matches("^Are you sure you want to log out[\\s\\S]$"));
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
